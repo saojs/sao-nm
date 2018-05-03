baa41da06cd6f336221ecc755728bea2ae463999
EGOIST <0x142857@gmail.com>
2018-05-03 23:44:56 +0800

    tweaks

--- generators/donation/saofile.js
+++ generators/donation/saofile.js
@@ -8,7 +8,7 @@ module.exports = {
       required: true
     },
     {
-      name: 'donateUrl',
+      name: 'url',
       message: 'The URL where users can donate to your project',
       store: true,
       default: 'none',
@@ -17,18 +17,24 @@ module.exports = {
   ],
   actions() {
     return [
-      {
+      this.answers.url !== 'none' && {
         type: 'modify',
         files: 'package.json',
         handler: data => {
-          data.scripts.postinstall = `node -e \"console.log('\\u001b[35m\\u001b[1mLove ${this.answers.name}? You can now donate to support the author:\\u001b[22m\\u001b[39m\\n> \\u001b[36mhttps://patreon.com/egoist\\u001b[39m')\"`
+          data.scripts = Object.assign({}, data.scripts, {
+            postinstall: `node -e \"console.log('\\u001b[35m\\u001b[1mLove ${this.answers.name}? You can now donate to support the author:\\u001b[22m\\u001b[39m\\n> \\u001b[36m${this.answers.url}\\u001b[39m')\"`
+          })
           return data
         }
       }
-    ]
+    ].filter(Boolean)
   },
   async complete() {
-    this.logger.success('Added "postinstall" script in package.json!')
-    this.logger.tip(`Run ${this.color.cyan(`${await this.determinePackageManager()} run postinstall`)} to see the effect.`)
+    if (this.answers.url === 'none') {
+      this.logger.warn('Skipped!')
+    } else {
+      this.logger.success('Added "postinstall" script in package.json!')
+      this.logger.tip(`Run ${this.color.cyan(`${await this.determinePackageManager()} run postinstall`)} to see the effect.`)
+    }
   }
 }
