ec05ed840618f0eec81da6e45430ccdb88f0ef17
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-01 01:16:22 +0800

    Delete template directory

--- template/src/index.js
+++
@@ -1,7 +0,0 @@
-export default function (input, options = {}) {
-  if (typeof input !== 'string') {
-    throw new Error('Expected input to be string')
-  }
-
-  return `${input} & ${options.postfix || 'rainbow'}`
-}
--- template/test/index.test.js
+++
@@ -1,9 +0,0 @@
-<%_ if (compile) { -%>
-import <%= this.camelcase(name) %> from '../src'
-<%_ } else { -%>
-const <%= this.camelcase(name) %> = require('../')
-<%_ } -%>
-
-test('main', () => {
-  expect(typeof <%= this.camelcase(name) %>).toBe('function')
-})
